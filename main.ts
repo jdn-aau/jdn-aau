fb2999687b9e8fe488f2c5f1474b53a996e04b5a
Jens Dalsgaard Nielsen <jdn@es.aau.dk>
2022-10-06 12:47:09 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,12 @@
-let right_speed = 0
-let left_speed = 0
 basic.showIcon(IconNames.SmallHeart)
 basic.forever(function () {
-    left_speed = randint(-40, 40)
-    right_speed = randint(-40, 40)
-    cuteBot.motors(left_speed, right_speed)
-    basic.pause(1000)
+    if (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line)) {
+        cuteBot.motors(50, 0)
+    }
+    if (cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline)) {
+        cuteBot.motors(0, 50)
+    }
+    if (cuteBot.tracking(cuteBot.TrackingState.L_R_line)) {
+        cuteBot.motors(50, 50)
+    }
 })
